5330a1fe71eb5ceca42582077b61659c1972a387
anand9773 <aaanand9773@gmail.com>
2024-07-09 18:59:29 +0530

    integrating Ether.js

--- contracts/DshopMart.sol
+++ contracts/DshopMart.sol
@@ -1,85 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
-// SPDX-License-Identifier: GPL-3.0
+contract DshopMart  {
+    address public owner;
 
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
-contract DshopMart is ReentrancyGuard{
-    address public onwer;
-
-    struct Product{
+    struct Item {
         uint256 id;
         string name;
         string category;
         string image;
-        uint256 price;
+        uint256 cost;
         uint256 rating;
         uint256 stock;
-
-
     }
 
-    struct Order{
+    struct Order {
         uint256 time;
-        Product product;
+        Item item;
     }
 
-    mapping (uint256 => Product) public products;
-    mapping (address => mapping (uint256 => Order)) public orders;
-    mapping (address => uint256) public orderCount;
+    mapping(uint256 => Item) public items;
+    mapping(address => mapping(uint256 => Order)) public orders;
+    mapping(address => uint256) public orderCount;
 
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+    event List(string name, uint256 cost, uint256 quantity);
 
-    event Buy(address customer, uint256 orderId, uint256 productId);
-    event List(string name, uint256 price, uint256 quantity);
-
-    modifier onlyOwner(){
-        require(msg.sender == onwer,"need owner authority!");
+    modifier onlyOwner() {
+        require(msg.sender == owner);
         _;
     }
 
-    constructor(){
-        onwer = msg.sender;
+    constructor() {
+        owner = msg.sender;
     }
 
-    function list( 
+    function list(
         uint256 _id,
         string memory _name,
         string memory _category,
         string memory _image,
-        uint256 _price,
+        uint256 _cost,
         uint256 _rating,
         uint256 _stock
     ) public onlyOwner {
-        products[_id] = Product(_id,_name,_category,_image,_price,_rating,_stock);
-        
-        emit List(_name, _price, _stock);
+        // Create Item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
 
+        // Add Item to mapping
+        items[_id] = item;
+
+        // Emit event
+        emit List(_name, _cost, _stock);
     }
 
     function buy(uint256 _id) public payable {
-        Product memory _product = products[_id];
+        // Fetch item
+        Item memory item = items[_id];
 
-        require(msg.value >= _product.price);
-        require(_product.stock >= 1);
-        Order memory _order = Order(block.timestamp, _product);
-        orderCount[msg.sender]++;
-        orders[msg.sender][orderCount[msg.sender]] = _order;
+        // Require enough ether to buy item
+        require(msg.value >= item.cost);
 
-        products[_id].stock = _product.stock- 1;
+        // Require item is in stock
+        require(item.stock > 0);
 
-        emit Buy(msg.sender, orderCount[msg.sender], _product.id);
+        // Create order
+        Order memory order = Order(block.timestamp, item);
 
+        // Add order for user
+        orderCount[msg.sender]++; // <-- Order ID
+        orders[msg.sender][orderCount[msg.sender]] = order;
 
+        // Subtract stock
+        items[_id].stock = item.stock - 1;
 
+        // Emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
     }
 
-    function withdraw() public onlyOwner nonReentrant{
-        uint256 balance = address(this).balance;
-        require(balance > 0, "No found to withdraw ");
-        (bool success,) = onwer.call{value:balance}("");
-        require(success,"Transfer failed");
-        
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
     }
-
 }
